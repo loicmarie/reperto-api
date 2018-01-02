3fd42df0776a5a129ada0546f7788e660197ee6e
Loïc MARIE <loic.marie54@gmail.com>
2018-01-02 19:15:25 +0100

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/hello/:name', (req, res) => {
   res.status(200).send(req.params);
 })
 
-app.get('*', (req, res) => {
+app.get('[^index.html]*', (req, res) => {
   res.redirect('/index.html');
 })
 
