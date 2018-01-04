42c92ef516289bfa0c85bfc2542dc6b7b148dedc
Loïc MARIE <loic.marie54@gmail.com>
2018-01-04 19:16:27 +0100

    update

--- server.js
+++ server.js
@@ -16,68 +16,15 @@ log.level('debug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('../front/dist/'));
 app.use('/api', routes.api);
 
-log.debug('root:', path.join( __dirname, '../'));
-
-app.get('*', (req, res, next) => {
-  res.sendFile('/front/dist/index.html', { root: path.join( __dirname, '../') });
-});
+if (process.env.REPERTO_ENV == 'prod') {
+  app.use(express.static('../front/dist/'));
+  app.get('*', (req, res, next) => {
+    res.sendFile('/front/dist/index.html', { root: path.join( __dirname, '../') });
+  });
+}
 
 app.listen(port, () => {
   log.info('Listening on port', port);
 });
-
-// let restify = require('restify');
-// let fs = require('fs');
-//
-// let users = require('./controller/users');
-// let variants = require('./controller/variants');
-// let pgn = require('./controller/pgn');
-//
-// let server = restify.createServer();
-// server.use(restify.plugins.bodyParser());
-//
-// // Users CRUD
-// server.get('/users', users.list);
-// server.post('/users', users.create);
-// server.get('/users/:id', users.get);
-// server.post('/users/:id', users.update);
-// server.del('/users/:id', users.delete);
-// // Users Utils
-// server.get('/users/uid/:id', users.getFromUserId);
-//
-// // Variants CRUD
-// server.get('/variants', variants.list);
-// server.post('/variants', variants.create);
-// server.get('/variants/:id', variants.get);
-// server.post('/variants/:id', variants.update);
-// server.del('/variants/:id', variants.delete);
-//
-// server.post('/pgn/tojson', pgn.toJSON);
-//
-//
-// let serveIndex = (req, res, next) => {
-//   console.log(req, res, next);
-//   fs.readFile(__dirname + '/index.html', (err, data) => {
-//     if (err) {
-//       next(err);
-//       return;
-//     }
-//
-//     res.setHeader('Content-Type', 'text/html');
-//     res.writeHead(200);
-//     res.end(data);
-//     next();
-//   });
-//
-// }
-//
-// // server.get('/', serveIndex);
-//
-// server.on('NotFound', serveIndex);
-//
-// server.listen(8080, function() {
-//   console.log('%s listening at %s', server.name, server.url);
-// });
